03efbf67b6b158c0f295e392099287cabde81982
Kevin <kevin11068@gmail.com>
2015-12-15 00:56:39 +0800

    Stylize error message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	def devise_error_message!
+		return "" if resources.errors.empty?
+			message = resources.error.full_messages.map {
+				|msg| content_tag(:li, msg) }.join
+
+			html = <<-HTML
+
+		<div class="alert alert-error alert-danger">
+			<a href="#" class= "close" data-dismiss="alert" >&#215;</a>
+			<%= content_tag :div, msg if msg.is_a?(String) %>
+		</div>
+
+		HTML
+		 	html.html_safe
+	end	
 end
