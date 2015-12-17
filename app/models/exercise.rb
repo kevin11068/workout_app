dba566e5b46d5de297fed2f2a12fa1ec059c86da
Kevin <kevin11068@gmail.com>
2015-12-17 09:09:09 +0800

    add workout charts

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -7,4 +7,5 @@ validates :workout, presence: true
 validates :workout_date, presence: true
 validates :user_id, presence: true
 
+default_scope { where('workout_date > ?', 15.days.ago).order(workout_date: :desc) }
 end
