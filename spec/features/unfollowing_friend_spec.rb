9a42cc62bc122101c8c42a4fdb44322bb2e9e64b
Kevin <kevin11068@gmail.com>
2015-12-21 10:56:18 +0800

    unfollow a friend

---
+++ spec/features/unfollowing_friend_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.feature "Unfollow friend" do
+	before do 
+		@john = User.create(first_name: "John", last_name: "Smith", email: "john@example.com", password: "password")
+		@sarah = User.create(first_name: "Sarah", last_name: "Doe", email: "sarah@example.com", password: "password")
+		
+
+		login_as(@john)
+
+		@following = Friendship.create(user: @john, friend: @sarah)
+	end
+
+	scenario do
+		visit "/"
+		click_link "My Lounge"
+		#find(:xpath, "//link[href='/friendship/#{@following.id}']").click
+
+		link = "a[href='/friendships/#{@following.id}'][data-method='delete']"
+		#link = "a[href='/friendship/#{@following.id}']"
+
+		find(link).click
+		expect(page).to have_content(@sarah.full_name + " unfollowed")
+	end
+end 
